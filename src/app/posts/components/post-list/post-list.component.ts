7d002683fc571fa386f53031fa45c67240cf162c
Mariquena Allosa <allosamariquena@gmail.com>
2023-12-18 17:22:06 -0300

    Listar Posts en uns grilla paginada y filtrada

--- src/app/posts/components/post-list/post-list.component.ts
+++ src/app/posts/components/post-list/post-list.component.ts
@@ -1,75 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { PostService } from 'src/app/core/services/post.service';
 import { Post } from 'src/app/core/models/post';
-import {  ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
-  
 })
 export class PostListComponent implements OnInit {
-  // Propiedades del componente
   posts: Post[] = [];
-  page: number = 1 ;
-  currentPage: number = 1;
-  itemsPerPage: number = 1;
+  filteredPosts: Post[] = [];
+  page: number = 1;
   searchTerm: string = '';
 
-
-// Constructor del componente con inyección de dependencias
-  constructor(private postService: PostService,private route : ActivatedRoute ,private router: Router){}
-
+  constructor(
+    private postService: PostService,
+    private cdr: ChangeDetectorRef,
+    private router: Router,
+    private route : ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    // Observar cambios en los parámetros de la URL
-    this.route.queryParams.subscribe((params) => {
-    // Obtener valor de término de búsqueda de la URL
-      this.page = params['page'] || 1;
-      this.searchTerm = params['searchTerm'] || '';
-    // Cargar posts según la configuración actual
-      this.loadPosts();
-    });
+    this.loadPosts();
   }
- // Cargar los posts
+
   loadPosts() {
-    this.postService.getPosts(this.page).subscribe((result) => {
-       // Actualizar la lista de posts en el componente
-      this.posts = result;
-      console.log("Posts cargados correctamente: ", this.posts)
-    }, err => console.log("Error al cargar los posts: ", err));
+    this.postService.getPosts(this.page).subscribe(
+      (posts) => {
+        this.posts = posts;
+        this.applyFilter();
+        console.log("Posts cargados correctamente: ", this.posts);
+      },
+      (err) => console.log("Error al cargar los posts: ", err)
+    );
   }
 
-  // Manejar cambio de página
-  onPageChange(pageNumber: number):void{
-    this.page = pageNumber
+  onPageChange(pageNumber: number) {
+    this.page = pageNumber;
     this.updateUrl();
-    this.loadPosts();
   }
 
-  // Manejar cambios en el término de búsqueda
   onSearchChange(): void {
+    // Reiniciar la lista de posts
+    this.posts = [];
     // Resetear la página a 1, actualizar la URL y recargar los posts
+    this.page = 1;
     this.updateUrl();
     this.loadPosts();
   }
 
-  // Método para actualizar la URL con los parámetros actuales
-  updateUrl(): void {
+  applyFilter(): void {
+    // Filtra los posts por título usando el texto de búsqueda
+    this.posts = this.posts.filter((post) =>
+      post.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  updateUrl() {
     const queryParams = {
       page: this.page,
       searchTerm: this.searchTerm || null,
     };
 
-    // Actualizar la URL manteniendo otros parámetros de la consulta
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams,
       queryParamsHandling: 'merge',
     });
   }
-
-
-
 }
+
