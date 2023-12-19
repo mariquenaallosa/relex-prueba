fe10ff72dafd93682b5f8cab5708368ccce1c17f
Mariquena Allosa <allosamariquena@gmail.com>
2023-12-19 02:18:34 -0300

    Mejoras en los filtro de búsqueda

--- src/app/posts/components/post-list/post-list.component.ts
+++ src/app/posts/components/post-list/post-list.component.ts
@@ -8,54 +8,52 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
+
 export class PostListComponent implements OnInit {
-  posts: Post[] = [];
+  allPosts: Post[] = [];
   filteredPosts: Post[] = [];
   page: number = 1;
   searchTerm: string = '';
 
   constructor(
     private postService: PostService,
-    private cdr: ChangeDetectorRef,
     private router: Router,
-    private route : ActivatedRoute
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
     this.loadPosts();
+
   }
 
   loadPosts() {
     this.postService.getPosts(this.page).subscribe(
       (posts) => {
-        this.posts = posts;
+        this.allPosts = posts;
         this.applyFilter();
-        console.log("Posts cargados correctamente: ", this.posts);
+        console.log('Posts cargados correctamente: ', this.allPosts);
       },
-      (err) => console.log("Error al cargar los posts: ", err)
+      (err) => console.log('Error al cargar los posts: ', err)
     );
   }
 
   onPageChange(pageNumber: number) {
     this.page = pageNumber;
     this.updateUrl();
-  }
-
-  onSearchChange(): void {
-    // Reiniciar la lista de posts
-    this.posts = [];
-    // Resetear la página a 1, actualizar la URL y recargar los posts
-    this.page = 1;
-    this.updateUrl();
     this.loadPosts();
   }
 
+  onSearchChange(): void {
+    this.page = 1; 
+    this.updateUrl();
+    this.applyFilter();
+  }
+
   applyFilter(): void {
-    // Filtra los posts por título usando el texto de búsqueda
-    this.filteredPosts = this.posts.filter((post) =>
+    // Filtra los posts por título usando el texto de búsqueda en la lista completa
+    this.filteredPosts = this.allPosts.filter((post) =>
       post.title.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
-    this.posts = this.filteredPosts
   }
 
   updateUrl() {
@@ -70,5 +68,6 @@ export class PostListComponent implements OnInit {
       queryParamsHandling: 'merge',
     });
   }
-}
 
+
+}
