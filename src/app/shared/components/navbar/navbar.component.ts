f8da88e95eff2b3677d768e99e648210ad141cc9
Mariquena Allosa <allosamariquena@gmail.com>
2023-12-20 01:37:42 -0300

    Corrección links

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,26 +6,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  page: number = 1;
-  searchTerm: string = '';
 
 
-  constructor(
-    private router: Router,
-    private route: ActivatedRoute,
-  ) {}
-
-  updateUrl() {
-    const queryParams = {
-      page: this.page,
-      searchTerm: this.searchTerm || null,
-    };
-
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams,
-      queryParamsHandling: 'merge',
-    });
-  }
 
 }
